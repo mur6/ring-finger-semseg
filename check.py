49ae888dcfb69864558d207b5e8e11cbe2f5e6c7
MURAKI Taichi <muraki.taichi@gmail.com>
2022-09-14 19:01:11 +0900

    fix

--- check.py
+++ check.py
@@ -25,3 +25,114 @@ train_dataset = ImageSegmentationDataset(
 valid_dataset = ImageSegmentationDataset(
     root_dir=base_data_dir / "validation", feature_extractor=feature_extractor, transforms=None, train=False
 )
+
+encoded_inputs = train_dataset[0]
+print(encoded_inputs["pixel_values"].shape)
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+valid_dataloader = DataLoader(valid_dataset, batch_size=4)
+
+# classes = pd.read_csv('./drone_dataset/class_dict_seg.csv')['name']
+id2label = {0: "unlabeled", 1: "hand", 2: "mat"}
+# id2label = classes.to_dict()
+label2id = {v: k for k, v in id2label.items()}
+
+print(label2id)
+
+# import matplotlib.pyplot as plt
+
+# mask = encoded_inputs["labels"].numpy()
+# plt.imshow(mask)
+# plt.show()
+
+model = SegformerForSemanticSegmentation.from_pretrained(
+    "nvidia/mit-b1",
+    ignore_mismatched_sizes=True,
+    num_labels=len(id2label),
+    id2label=id2label,
+    label2id=label2id,
+    reshape_last_stage=True,
+)
+import torch
+from sklearn.metrics import accuracy_score
+from tqdm.notebook import tqdm
+from transformers import AdamW
+
+optimizer = AdamW(model.parameters(), lr=0.00006)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+print("Model Initialized!")
+
+for epoch in range(1, 15 + 1):  # loop over the dataset multiple times
+    print("Epoch:", epoch)
+    pbar = tqdm(train_dataloader)
+    accuracies = []
+    losses = []
+    val_accuracies = []
+    val_losses = []
+    model.train()
+    for idx, batch in enumerate(pbar):
+        # get the inputs;
+        pixel_values = batch["pixel_values"].to(device)
+        labels = batch["labels"].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        outputs = model(pixel_values=pixel_values, labels=labels)
+
+        # evaluate
+        upsampled_logits = nn.functional.interpolate(
+            outputs.logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
+        )
+        predicted = upsampled_logits.argmax(dim=1)
+
+        mask = labels != 0  # we don't include the background class in the accuracy calculation
+        # print(mask)
+        pred_labels = predicted[mask].detach().cpu().numpy()
+        true_labels = labels[mask].detach().cpu().numpy()
+        accuracy = accuracy_score(pred_labels, true_labels)
+        loss = outputs.loss
+        accuracies.append(accuracy)
+        losses.append(loss.item())
+        pbar.set_postfix(
+            {"Batch": idx, "Pixel-wise accuracy": sum(accuracies) / len(accuracies), "Loss": sum(losses) / len(losses)}
+        )
+
+        # backward + optimize
+        loss.backward()
+        optimizer.step()
+    else:
+        model.eval()
+        with torch.no_grad():
+            for idx, batch in enumerate(valid_dataloader):
+                pixel_values = batch["pixel_values"].to(device)
+                labels = batch["labels"].to(device)
+
+                outputs = model(pixel_values=pixel_values, labels=labels)
+                upsampled_logits = nn.functional.interpolate(
+                    outputs.logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
+                )
+                predicted = upsampled_logits.argmax(dim=1)
+
+                mask = labels != 0  # we don't include the background class in the accuracy calculation
+                pred_labels = predicted[mask].detach().cpu().numpy()
+                true_labels = labels[mask].detach().cpu().numpy()
+                accuracy = accuracy_score(pred_labels, true_labels)
+                val_loss = outputs.loss
+                val_accuracies.append(accuracy)
+                val_losses.append(val_loss.item())
+    # writer.add_scalar('Loss/train', sum(losses)/len(losses), epoch)
+    # writer.add_scalar('Loss/val', sum(val_losses)/len(val_losses), epoch)
+    # writer.add_scalar('Accuracy/train', sum(accuracies)/len(accuracies), epoch)
+    # writer.add_scalar('Accuracy/val', sum(val_accuracies)/len(val_accuracies), epoch)
+    print(
+        f"Train Pixel-wise accuracy: {sum(accuracies)/len(accuracies)}\
+         Train Loss: {sum(losses)/len(losses)}\
+         Val Pixel-wise accuracy: {sum(val_accuracies)/len(val_accuracies)}\
+         Val Loss: {sum(val_losses)/len(val_losses)}"
+    )
+# writer.flush()
---
+++ generate_data.py
@@ -0,0 +1,132 @@
+import argparse
+import random
+import time
+from pathlib import Path
+from typing import List
+
+import albumentations as A
+import cv2
+import numpy as np
+
+noise_t = A.Compose(
+    [
+        A.GaussNoise(),
+        A.Blur(blur_limit=3),
+        # A.OpticalDistortion(),
+        # A.GridDistortion(),
+        A.OneOf(
+            [
+                A.Cutout(num_holes=40, max_h_size=4, max_w_size=4),
+                A.Cutout(num_holes=35, max_h_size=8, max_w_size=8),
+                A.Cutout(num_holes=20, max_h_size=16, max_w_size=16),
+            ],
+            p=0.8,
+        ),
+    ]
+)
+
+
+def get_mat_image(*, imsize):
+    image_file = Path("data/images/mat_high.jpeg")
+    # img = cv2.imread(str(image_file), 0)
+    img = cv2.imread(str(image_file), cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, imsize)
+    return img
+
+
+def make_random_four_points(*, rho, imsize):
+    x = random.randint(0, rho)
+    y = random.randint(0, rho)
+    yield x, y
+    x = imsize - random.randint(0, rho)
+    y = random.randint(0, rho)
+    yield x, y
+    x = imsize - random.randint(0, rho)
+    y = imsize - random.randint(0, rho)
+    yield x, y
+    x = random.randint(0, rho)
+    y = imsize - random.randint(0, rho)
+    yield x, y
+
+
+class PreProcess:
+    imsize = (IM_SIZE, IM_SIZE)
+    original_four_points = np.float32([(0, 0), (IM_SIZE, 0), (IM_SIZE, IM_SIZE), (0, IM_SIZE)])
+    original_four_points_numpy = np.array(original_four_points)
+    mat_image = get_mat_image(imsize=imsize)
+
+    def __init__(
+        self, new_path: Path, data_size: int, background_images: List[np.ndarray], generate_image: bool
+    ) -> None:
+        if not new_path.exists():
+            new_path.mkdir(parents=True)
+        self.new_path = new_path
+        self.data_size = data_size
+        self.rho = RHO
+        self.background_images = background_images
+        self.generate_image = generate_image
+
+    def make_warped_image(self):
+        # img = self.mat_image.copy()
+        perturbed_four_points = list(make_random_four_points(rho=self.rho, imsize=self.imsize[0]))
+        H_inverse = cv2.getPerspectiveTransform(self.original_four_points, np.float32(perturbed_four_points))
+        dst = get_random_background(self.background_images)
+        warped_image = cv2.warpPerspective(
+            self.mat_image, H_inverse, self.imsize, borderMode=cv2.BORDER_TRANSPARENT, dst=dst
+        )
+        warped_image = noise_t(image=warped_image)["image"]
+        return warped_image, perturbed_four_points
+
+    def get_datum(self, warped_image, perturbed_four_points):
+        # training_image = np.dstack((self.mat_image, warped_image))
+        training_image = warped_image
+        # print(training_image.shape)
+        perturbed_four_points_numpy = np.array(perturbed_four_points)
+        H_four_points = np.subtract(perturbed_four_points_numpy, self.original_four_points_numpy)
+        return (training_image, perturbed_four_points_numpy, H_four_points)
+
+    def __call__(self):
+        # save .npy files
+        # print("Generate {} {} files from {} raw data...".format(data_size, new_path, len(filenames)))
+        for i in range(self.data_size):
+            warped_image, perturbed_four_points = self.make_warped_image()
+            d = self.get_datum(warped_image, perturbed_four_points)
+            np.save(self.new_path / f"{i:06d}", d)
+            if self.generate_image:
+                cv2.imwrite(str(self.new_path / f"{i:06d}.jpeg"), warped_image)
+
+
+def main(args):
+    start = time.time()
+
+    output_dir = Path("data/outputs/")
+
+    # kwargs = dict(
+    #     background_images=init_background_images(args.background_image_path), generate_image=args.generate_image
+    # )
+
+    # PreProcess(output_dir / "training/", data_size=args.training_data_size, **kwargs)()
+    # PreProcess(output_dir / "validation/", data_size=args.validation_data_size, **kwargs)()
+    # PreProcess(output_dir / "testing/", data_size=args.testing_data_size, **kwargs)()
+
+    elapsed_time = time.time() - start
+    print(
+        "Generate dataset in {:.0f}h {:.0f}m {:.0f}s.".format(
+            elapsed_time // 3600,
+            (elapsed_time % 3600) // 60,
+            (elapsed_time % 3600) % 60,
+        )
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--background_image_path", type=Path, default="/content/data")
+    parser.add_argument("--training_data_size", type=int, default=100, help="training data_size")
+    parser.add_argument("--validation_data_size", type=int, default=10, help="validation data_size")
+    parser.add_argument("--testing_data_size", type=int, default=10, help="testing data_size")
+    parser.add_argument("--generate_image", type=bool, default=False, help="Generate jpeg image or not")
+    args = parser.parse_args()
+    print(args)
+    main(args)
