7fc7ee6b52d885f94c834224d15c31cf81464a3f
MURAKI Taichi <muraki.taichi@gmail.com>
2022-09-15 17:17:52 +0900

    推論を行うスクリプト

---
+++ inf_segformer.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from PIL import Image
+from transformers import SegformerFeatureExtractor, SegformerForSemanticSegmentation
+
+
+def get_images(samples_dir):
+    samples_dir = Path(samples_dir)
+
+    def _iter_pil_images():
+        sample_images = sorted(list(samples_dir.glob("*.jpg")))
+        for p in sample_images:
+            image = Image.open(p)
+            yield image
+
+    return tuple(_iter_pil_images())
+
+
+images = get_images("data/samples")
+
+# feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/mit-b0")
+feature_extractor_inference = SegformerFeatureExtractor(do_random_crop=False, do_pad=False)
+# model = SegformerForImageClassification.from_pretrained("nvidia/mit-b0")
+
+model_dir = sys.argv[1]
+save_file_name = sys.argv[2]
+model = SegformerForSemanticSegmentation.from_pretrained(model_dir)
+model.eval()
+inputs = feature_extractor_inference(images=images, return_tensors="pt")
+outputs = model(**inputs)
+# shape (batch_size, num_labels, height, width)
+
+logits = outputs.logits
+
+print(f"outputs: {type(outputs)}")
+print(f"outputs.logis: {logits.shape}")
+
+torch.save(logits, save_file_name)
